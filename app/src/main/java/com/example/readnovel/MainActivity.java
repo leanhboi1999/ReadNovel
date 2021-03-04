7c5daebe5bf16f722a0ba43b7b239b232504ba5b
Le Duc Anh <leanhboi1999@outlook.com>
2021-03-04 14:42:14 +0700

    Update permission

--- app/src/main/java/com/example/readnovel/MainActivity.java
+++ app/src/main/java/com/example/readnovel/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.readnovel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
@@ -9,38 +14,64 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+//import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView text_first;
     EditText username, password;
     Button button_first, button_second;
     String username_text, password_text;
+
+    //ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    //Find id control
-    text_first = findViewById(R.id.text_first);
-    text_first.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimension(R.dimen.size_14sp));
-    text_first.setText("Đăng nhập");
-    username = findViewById(R.id.username);
-    password = findViewById(R.id.password);
 
-    button_first = findViewById(R.id.button_first);
-    button_first.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            login(view);
+        //Check Permission
+        int permission_internet = ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
+        int permission_read_external_storage = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        int permission_write_external_storage = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission_internet != PackageManager.PERMISSION_GRANTED || permission_read_external_storage != PackageManager.PERMISSION_GRANTED || permission_write_external_storage != PackageManager.PERMISSION_GRANTED) {
+            makeRequest();
         }
-    });
+        //Find id control
+        text_first = findViewById(R.id.text_first);
+        text_first.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimension(R.dimen.size_14sp));
+        text_first.setText("Đăng nhập");
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+
+        //Load image
+        //image = findViewById(R.id.Image);
+
+        button_first = findViewById(R.id.button_first);
+        button_first.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login(view);
+            }
+        });
+    }
+
+    private void makeRequest() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
     }
 
     public void login(View view) {
-        Log.v("Click", "Button clicked");
+        username_text = username.getText().toString();
+        password_text = password.getText().toString();
+
+        Intent i = new Intent(this, Dashboard.class);
+        i.putExtra("email", username_text);
+        i.putExtra("password", password_text);
+        startActivity(i);
+
         //Creating a message toast
-        Toast.makeText(this, "Button is clicked", Toast.LENGTH_LONG).show();
-        /*username_text = username.getText().toString();
-        password_text = password.getText().toString();*/
+        //Toast.makeText(this, "Button is clicked", Toast.LENGTH_LONG).show();
+
+        //Change image when click button
+        //image.setImageResource(R.drawable.nameImage);
     }
 }
