c168ad354463ae0e95616b71d909178135331344
Dang Thi Thuy NHI <dangtthuynhi@gmail.com>
2021-05-09 21:24:18 +0700

    test thử github

--- app/src/main/java/com/example/readnovel/Dashboard.java
+++ app/src/main/java/com/example/readnovel/Dashboard.java
@@ -18,7 +18,7 @@ public class Dashboard extends AppCompatActivity {
         String name = i.getStringExtra("email");
 
         //Display text
-        text.setText("Welcome to " + name);;;
+        text.setText("Welcome to " + name);
 
     }
 }
