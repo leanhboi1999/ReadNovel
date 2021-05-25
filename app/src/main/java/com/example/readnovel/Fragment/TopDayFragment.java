85929bf1f5f14ff82cb15a87091daaf643d3bc97
Nhi <Dangtthuynhi@gmail.com>
2021-05-25 11:56:48 +0700

    Thêm TopDay
    TopDayFragment, fragment_top_day

---
+++ app/src/main/java/com/example/readnovel/Fragment/TopDayFragment.java
@@ -0,0 +1,287 @@
+package com.example.readnovel.Fragment;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.readnovel.Adapter.LoadMoreAdapter;
+import com.example.readnovel.Model.Comic;
+import com.example.readnovel.R;
+import com.example.readnovel.Untils.Link;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+
+import jp.co.recruit_lifestyle.android.widget.WaveSwipeRefreshLayout;
+
+public class TopDayFragment extends Fragment {
+    private LinearLayout mNoPost;
+    private RecyclerView mNgayRv;
+
+    private Boolean isScrolling = false;
+    private int currentItems;
+    private int totalItems;
+    private int scrollOutItems;
+    private LinearLayoutManager manager;
+    private int pos = 0;
+    private ArrayList<String> lstUrl;
+    private ArrayList<Comic> lstmore;
+    private ArrayList<Comic> lstBXH;
+    private LoadMoreAdapter adapterBXH;
+    private WaveSwipeRefreshLayout mSwipeMain;
+
+    private void initView(@NonNull final View itemView) {
+        lstBXH = new ArrayList<>();
+        lstUrl = new ArrayList<>();
+        lstmore = new ArrayList<>();
+        mNoPost = itemView.findViewById(R.id.noPost);
+        mNgayRv = itemView.findViewById(R.id.rv_Ngay);
+        adapterBXH = new LoadMoreAdapter(getContext(), lstBXH);
+        manager = new LinearLayoutManager(getContext());
+        mSwipeMain = itemView.findViewById(R.id.swipeRefreshLayout);
+    }
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_top_day, container, false);
+        initView(view);
+
+        loadBXH();
+
+        mNgayRv.setLayoutManager(manager);
+        mNgayRv.setHasFixedSize(true);
+        mNgayRv.setItemAnimator(new DefaultItemAnimator());
+        mNgayRv.setAdapter(adapterBXH);
+        mSwipeMain.setOnRefreshListener(new WaveSwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshItems();
+            }
+        });
+
+        mNgayRv.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                    isScrolling = true;
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                currentItems = manager.getChildCount();
+                totalItems = manager.getItemCount();
+                scrollOutItems = manager.findFirstVisibleItemPosition();
+
+                if (isScrolling && (currentItems + scrollOutItems == totalItems)) {
+                    isScrolling = false;
+                    if (pos <= lstUrl.size() - 1) {
+                        loadBookmore(lstUrl.get(pos));
+                    }
+                }
+            }
+        });
+        return view;
+    }
+
+    private void refreshItems() {
+        loadBXH();
+
+    }
+
+    private void onItemsLoadComplete() {
+        mSwipeMain.setRefreshing(false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (lstBXH.size() > 0) {
+            mNoPost.setVisibility(View.GONE);
+        } else {
+            mNoPost.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    private void loadBookmore(final String url) {
+        pos++;
+        lstmore.clear();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Document document = Jsoup.parse(response);
+                        Elements all = document.select("div#ctl00_divCenter");
+                        Elements sub = all.select(".item");
+                        for (Element element : sub) {
+                            Element hinhanh = element.getElementsByTag("img").get(0);
+                            Element linktruyen = element.getElementsByTag("a").get(0);
+                            Element sochuong = element.getElementsByTag("a").get(2);
+                            Element tentruyen = element.getElementsByTag("h3").get(0);
+                            Element luotxem = element.getElementsByTag("span").get(0);
+                            Element luotxem2 = null;
+                            try {
+                                luotxem2 = element.getElementsByTag("span").get(1);
+                            } catch (Exception ex) {
+                                ex.printStackTrace();
+                            }
+                            String thumb;
+                            String thumb1 = hinhanh.attr("src");
+                            String thumb2 = hinhanh.attr("data-original");
+                            if (thumb2.equals("")) {
+                                thumb = thumb1;
+                            } else {
+                                thumb = thumb2;
+                            }
+                            String name = tentruyen.text();
+                            String link = linktruyen.attr("href");
+                            String view;
+                            if (luotxem.text().equals("")) {
+                                view = luotxem2.text();
+                            } else {
+                                view = luotxem.text();
+                            }
+                            String string = view;
+                            String[] parts = string.split(" ");
+                            String viewCount = parts[0];
+                            if (thumb.startsWith("http:") || thumb.startsWith("https:")) {
+
+                            } else {
+                                thumb = "http:" + thumb;
+                            }
+                            String chapter = sochuong.text();
+                            lstmore.add(new Comic(name, viewCount, thumb, chapter, link));
+                        }
+                        lstBXH.addAll(lstmore);
+                        adapterBXH.notifyDataSetChanged();
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                });
+                stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                        150000,
+                        DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                        DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+                requestQueue.add(stringRequest);
+            }
+        }).start();
+    }
+
+    private void loadBXH() {
+        lstBXH.clear();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, Link.URL_DAY, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Document document = Jsoup.parse(response);
+                        Elements all = document.select("div#ctl00_divCenter");
+                        Elements sub = all.select(".item");
+                        for (Element element : sub) {
+                            Element hinhanh = element.getElementsByTag("img").get(0);
+                            Element linktruyen = element.getElementsByTag("a").get(0);
+                            Element sochuong = element.getElementsByTag("a").get(2);
+                            Element tentruyen = element.getElementsByTag("h3").get(0);
+                            Element luotxem = element.getElementsByTag("span").get(0);
+                            Element luotxem2 = null;
+                            try {
+                                luotxem2 = element.getElementsByTag("span").get(1);
+                            } catch (Exception ex) {
+                                ex.printStackTrace();
+                            }
+                            String thumb;
+                            String thumb1 = hinhanh.attr("src");
+                            String thumb2 = hinhanh.attr("data-original");
+                            if (thumb2.equals("")) {
+                                thumb = thumb1;
+                            } else {
+                                thumb = thumb2;
+                            }
+                            String name = tentruyen.text();
+                            String link = linktruyen.attr("href");
+                            String view;
+                            if (luotxem.text().equals("")) {
+                                view = luotxem2.text();
+                            } else {
+                                view = luotxem.text();
+                            }
+                            String string = view;
+                            String[] parts = string.split(" ");
+                            String viewCount = parts[0];
+                            if (thumb.startsWith("http:") || thumb.startsWith("https:")) {
+
+                            } else {
+                                thumb = "http:" + thumb;
+                            }
+                            Log.e("name:", name);
+                            String chapter = sochuong.text();
+                            lstBXH.add(new Comic(name, viewCount, thumb, chapter, link));
+                        }
+
+                        Elements more = document.select("div#ctl00_mainContent_ctl01_divPager");
+                        Elements more1 = more.select("ul.pagination");
+                        Elements allli = more1.select("li");
+                        Elements next = allli.select("a");
+                        for (Element element : next) {
+                            String url = element.attr("href");
+                            lstUrl.add(url);
+                        }
+
+                        if (lstBXH.size() > 0) {
+                            mNoPost.setVisibility(View.GONE);
+                        } else {
+                            mNoPost.setVisibility(View.VISIBLE);
+                        }
+                        onItemsLoadComplete();
+                        adapterBXH.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                });
+                stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                        150000,
+                        DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                        DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+                requestQueue.add(stringRequest);
+            }
+        }).start();
+    }
+
+}
