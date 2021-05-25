7b4f3df3fa86fddcc38e34413b29f694490d7633
Nhi <Dangtthuynhi@gmail.com>
2021-05-25 11:56:10 +0700

    Thêm Detail và liên quan
    Thêm DetailComicActivity, ComicViewAdapter, activity_detail_comic, item_custom_image

--- app/src/main/java/com/example/readnovel/Adapter/ComicViewAdapter.java
+++ app/src/main/java/com/example/readnovel/Adapter/ComicViewAdapter.java
@@ -1,4 +1,66 @@
 package com.example.readnovel.Adapter;
 
-public class ComicViewAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.readnovel.Model.Image;
+import com.example.readnovel.R;
+
+import java.util.List;
+
+public class ComicViewAdapter extends RecyclerView.Adapter<ComicViewAdapter.ViewHolder> {
+
+    private final List<Image> lst;
+    private final Context mContext;
+
+    public ComicViewAdapter(Context context, List<Image> chapterList) {
+        this.lst = chapterList;
+        this.mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_custom_image, parent, false);
+
+        return new ViewHolder(itemView);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        final ImageView imgComic;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            imgComic = itemView.findViewById(R.id.photo_view);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        final Image item = lst.get(position);
+
+//        ImageLoader imageLoader = ImageLoader.getInstance(); // Get singleton instance
+//        imageLoader.displayImage(item.getUrl().toString(), holder.imgComic);
+
+        Glide.with(mContext).load(item.getUrl()).into(holder.imgComic);
+//        CustomPicasso.with(mContext)
+//                .load(item.getUrl().toString())
+//                .error(R.drawable.bgtest)
+//                .into(holder.imgComic);
+    }
+
+    @Override
+    public int getItemCount() {
+        return lst.size();
+    }
+
 }
